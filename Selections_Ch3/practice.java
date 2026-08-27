b7f443f005e414efb0bbeb2076aa3fdf35779220
Antxnio19 <jesustrejo7914@gmail.com>
2026-08-27 16:00:44 -0400

    new example solved

--- Selections_Ch3/practice.java
+++ Selections_Ch3/practice.java
@@ -1,6 +1,51 @@
+import java.util.Scanner;
 class practice {
+
     public static void main(String[] args){
-        double radius = 1;
-        System.out.println(radius > 0);
+        /**double radius = 1;
+        System.out.println(radius > 0); **/
+
+        //=========================================
+
+        //3.2.3 cannot be done
+        /** 
+        boolean b = true;
+        i = (int)b;
+
+        int i = 1;
+        boolean b = (boolean)i; */
+
+        //=========================================
+        /**3.3.1
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+        int y = input.nextInt();
+        int x = 0;
+
+        if(y > 0){
+            x = 1;
+        }
+        System.out.println("The value of x is: " + x + " and the value of y is: " + y);
+        **/
+
+        //==========================================
+
+        /** 3.3.2 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your score value between 0 - 100: ");
+        int score = input.nextInt();
+        System.out.println("Enter your pay: ");
+        double pay = input.nextInt();
+
+        if(score > 90){
+            double bonus = pay * .03;
+            pay = pay + bonus;
+        }
+
+        System.out.println("Your Pay is: " + pay);
+        */
+        //=========================================
+
+        
     }
 }
